ff3b619fe31d83b7d2dd8fd6d60a38d088f827ae
Omri Lotan <omri@hamadgera.com>
2015-09-06 21:43:22 +0300

    Refactor, rename

--- loadr.js
+++ loadr.js
@@ -1,33 +1,33 @@
 /*
 // Usage
 //
-// addResource("styles/text", @String css);
-// addResource("styles/rules", @Object styles);
-// addResource("styles/file", @String URL, @Function callback);
-// addResource("styles/files", @String URL, @Function callback);
-// addResource("script/text", @String script);
-// addResource("script/file", @String URL, @Function callback);
-// addResource("script/files", @String URL, @Function callback);
+// loadr("styles/text", @String css);
+// loadr("styles/rules", @Object styles);
+// loadr("styles/file", @String URL, @Function callback);
+// loadr("styles/files", @String URL, @Function callback);
+// loadr("script/text", @String script);
+// loadr("script/file", @String URL, @Function callback);
+// loadr("script/files", @String URL, @Function callback);
 //
 // Styles rules object example: {
 //     ".myDiv": "color:blue; border:1px solid green;"
 // }
 */
 
-var addResource = (function __addResource__ (doc) {
+var loadr = (function __loadr__ (doc) {
     "use strict";
-    var setArrtibute = (function addResource$_forkSetArrtibute (element) {
+    var setArrtibute = (function loadr$_forkSetArrtibute () {
             if (typeof document.createElement("div").setAttribute === "function") {
-                return function addResource$_setArrtibute (element, name, value) {
+                return function loadr$_setArrtibute (element, name, value) {
                     element.setAttribute(name, value);
                 };
             } else {
-                return function addResource$_setArrtibute (element, name, value) {
+                return function loadr$_setArrtibute (element, name, value) {
                     element[name] = value;
                 };
             }
         }()),
-        setAttributes = function addResource$_setAttributes (element, attributes) {
+        setAttributes = function loadr$_setAttributes (element, attributes) {
             var name;
             for (name in attributes) {
                 if (attributes.hasOwnProperty(name)) {
@@ -36,7 +36,7 @@ var addResource = (function __addResource__ (doc) {
             }
             return element;
         },
-        addRule = function addResource$_addRule (sheet, selector, rules) {
+        addRule = function loadr$_addRule (sheet, selector, rules) {
             if (typeof sheet.insertRule === "function") {
                 sheet.insertRule(selector + "{" + rules + "}", 1);
             } else if (typeof sheet.addRule === "function") {
@@ -46,7 +46,7 @@ var addResource = (function __addResource__ (doc) {
             }
         },
         CSS = {
-            create: function addResource$_CSS$create (url) {
+            create: function loadr$_CSS$create (url) {
                 var element = doc.createElement("link"),
                     attributes = {
                         "type": "text/css"
@@ -58,7 +58,7 @@ var addResource = (function __addResource__ (doc) {
                 }
                 return setAttributes(element, attributes);
             },
-            element: function addResource$_CSS$element (content) {
+            element: function loadr$_CSS$element (content) {
                 var element = document.createElement("style");
                 if (typeof content === "string") {
                     if (typeof element.styleSheet === "object") {
@@ -70,7 +70,7 @@ var addResource = (function __addResource__ (doc) {
                 doc.head.appendChild(element);
                 return element;
             },
-            rules: function addResource$_CSS$rules (content) {
+            rules: function loadr$_CSS$rules (content) {
                 var sheet,
                     name;
                 if (typeof doc.styleSheets === "object" &&
@@ -85,15 +85,15 @@ var addResource = (function __addResource__ (doc) {
                     }
                 }
             },
-            reference: function addResource$_CSS$reference (content, callback) {
+            reference: function loadr$_CSS$reference (content, callback) {
                 var element = CSS.create(content);
                 element.onload = callback;
                 doc.head.appendChild(element);
             },
-            references: function addResource$_CSS$references (array, callback) {
+            references: function loadr$_CSS$references (array, callback) {
                 var loaded = 0;
-                array.forEach(function addResource$_CSS$referencesArray (item) {
-                    CSS.reference(item, function addResource$_CSS$referencesAdd () {
+                array.forEach(function loadr$_CSS$referencesArray (item) {
+                    CSS.reference(item, function loadr$_CSS$referencesAdd () {
                         loaded++;
                         if (loaded === array.length && typeof callback === "function") {
                             callback();
@@ -103,7 +103,7 @@ var addResource = (function __addResource__ (doc) {
             }
         },
         JS = {
-            create: function addResource$_JS$create (url) {
+            create: function loadr$_JS$create (url) {
                 var element = doc.createElement("script"),
                     attributes = {
                         "type": "text/javascript"
@@ -114,7 +114,7 @@ var addResource = (function __addResource__ (doc) {
                 setAttributes(element, attributes);
                 return element;
             },
-            element: function addResource$_JS$element (content) {
+            element: function loadr$_JS$element (content) {
                 var element = JS.create();
                 try {
                     element.appendChild(doc.createTextNode(content));
@@ -123,15 +123,15 @@ var addResource = (function __addResource__ (doc) {
                 }
                 doc.head.appendChild(element);
             },
-            reference: function addResource$_JS$reference (content, callback) {
+            reference: function loadr$_JS$reference (content, callback) {
                 var element = JS.create(content);
                 element.onload = callback;
                 doc.head.appendChild(element);
             },
-            references: function addResource$_JS$references (array, callback) {
+            references: function loadr$_JS$references (array, callback) {
                 var loaded = 0;
-                array.forEach(function addResource$_JS$referencesArray (item) {
-                    JS.reference(item, function addResource$_JS$referencesAdd () {
+                array.forEach(function loadr$_JS$referencesArray (item) {
+                    JS.reference(item, function loadr$_JS$referencesAdd () {
                         loaded++;
                         if (loaded === array.length && typeof callback === "function") {
                             callback();
@@ -152,16 +152,14 @@ var addResource = (function __addResource__ (doc) {
                 file:  JS.reference,
                 files: JS.references
             }
-        },
-        exports = function addResource (type, content, callback) {
-            var action = [].shift.call(arguments).split("/");
-
-            if (typeof map[action[0]] === "object" &&
-                    typeof map[action[0]][action[1]] === "function") {
-                return map[action[0]][action[1]].apply(null, arguments);
-            }
-            throw new Error("\"addResource\" requires an valid type assigned." + (typeof type === "string" ? " \"" + type + "\" is not supported" : ""));
         };
+    return function loadr (/* type, content, callback */) {
+        var action = [].shift.call(arguments).split("/");
 
-    return exports;
+        if (typeof map[action[0]] === "object" &&
+                typeof map[action[0]][action[1]] === "function") {
+            return map[action[0]][action[1]].apply(null, arguments);
+        }
+        throw new TypeError("Invalid assignment");
+    };
 } (document));
